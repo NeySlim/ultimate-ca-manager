31a6dabbbe496a6ab3559dd44d5f535515fb0195
NeySlim <lio@pew.pet>
2026-02-08 10:28:17 +0100

    fix: Docker WebSocket worker_class (geventwebsocket)

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -11,7 +11,7 @@ backlog = 2048
 
 # Worker processes
 workers = int(os.getenv('GUNICORN_WORKERS', multiprocessing.cpu_count() * 2 + 1))
-worker_class = 'sync'
+worker_class = 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
 worker_connections = 1000
 timeout = 120
 keepalive = 5
